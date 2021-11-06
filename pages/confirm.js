63bec999fafcfa0e798dd2188bd91aaea277bc95
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2021-11-06 13:24:36 -0700

    Add Back Button

--- pages/confirm.js
+++ pages/confirm.js
@@ -3,6 +3,7 @@ import tw from 'tailwind-styled-components'
 import Map from './components/Map'
 import { useRouter } from 'next/router'
 import RideSelector from './components/RideSelector'
+import Link from 'next/link'
 
 const Confirm = () => {
   const router = useRouter()
@@ -53,6 +54,11 @@ const Confirm = () => {
 
   return (
     <Wrapper>
+      <ButtonContainer>
+        <Link href='/search'>
+          <BackButton src='https://img.icons8.com/ios-filled/50/000000/left.png' />
+        </Link>
+      </ButtonContainer>
       <Map
         pickupCoordinates={pickupCoordinates}
         dropoffCoordinates={dropoffCoordinates}
@@ -91,4 +97,12 @@ flex-1 flex flex-col h-1/2
 // good job Anthony
 const Wrapper = tw.div`
 flex h-screen flex-col
+`
+
+const ButtonContainer = tw.div`
+rounded-full absolute top-4 left-4 z-10 bg-white shadow-md cursor-pointer
+`
+
+const BackButton = tw.img`
+h-full object-contain 
 `
